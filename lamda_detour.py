0a028c18e2e03ee41e6728e7b569bdb1f31edfd1
mihaTrajbaric <miha.trajbaric@gmail.com>
2018-12-15 14:01:16 +0100

    bugfix

--- lamda_detour.py
+++ lamda_detour.py
@@ -55,14 +55,24 @@ def make_detour(Graph, lambda_list, lambda_subgraphs, src_node_ID, dst_node_ID):
 
     def return_edge():
         # return the edge to where it belongs
-        min_lambda = next(x for x in lambda_list if x > congestion)
+        try:
+            min_lambda = next(x for x in lambda_list if x > congestion)
+        except StopIteration:
+            if len(lambda_list) == 1:
+                # that is the case with specific lambda
+                min_lambda = lambda_list[0]
+            else:
+                raise StopIteration
         index = lambda_list.index(min_lambda)
         for subgraph in lambda_subgraphs[index:]:
             # creates edge, then assign back the attributes
-            id = subgraph.AddEdge(src_node_ID, dst_node_ID)
-            subgraph.AddFltAttrDatE(id, flow, "Flow")
-            subgraph.AddFltAttrDatE(id, capacity, "Capacity")
-            subgraph.AddFltAttrDatE(id, congestion, "Congestion")
+            try:
+                id = subgraph.AddEdge(src_node_ID, dst_node_ID)
+                subgraph.AddFltAttrDatE(id, flow, "Flow")
+                subgraph.AddFltAttrDatE(id, capacity, "Capacity")
+                subgraph.AddFltAttrDatE(id, congestion, "Congestion")
+            except:
+                pass
 
         # and to main graph
         id = Graph.AddEdge(src_node_ID, dst_node_ID)
@@ -84,7 +94,6 @@ def make_detour(Graph, lambda_list, lambda_subgraphs, src_node_ID, dst_node_ID):
         #    print lambda_,path_length
     return_edge()
     return -1, 0
-
 def detour_without_lambda(Graph, src_node_ID, dst_node_ID):
     """
     makes a detour just with path_finding algorithm, without lambda optimization
@@ -138,17 +147,22 @@ def subgraph_statistics(graph_name="Anaheim"):
 
 
 
-def test_detouring(lambda_list, graph_name="Anaheim", n_of_tests=100):
+def test_detouring(lambda_list, graph_name="Anaheim", n_of_tests=100,specific_lambda = 1.0):
     G, graphs, _ = read(graph_name, lambda_list)
     assert len(graphs) == len(lambda_list)
     # print lambda_list
     distrib = []
     no_path = 0
-    path_length_without_lambda = 0
+    path_length_without_lambda = 0.0
     path_length_with_lambda = 0.0
-    total_time_without_lambda = 0
+    path_length_specific_lambda = 0.0
+    total_time_without_lambda = 0.0
     total_time_with_lambda = 0.0
+    total_time_specific_lambda = 0.0
     path_list = []
+
+    specifi_lambda_idx = lambda_list.index(specific_lambda)
+    specific_subgraph = graphs[specifi_lambda_idx]
 
     for i in range(n_of_tests):
         # pick random edge
@@ -159,6 +173,9 @@ def test_detouring(lambda_list, graph_name="Anaheim", n_of_tests=100):
 
         # src_id = 896
         # dst_id = 899
+        start = time.time()
+        _, path_l_temp_specific = make_detour(G, [specific_lambda], [specific_subgraph], src_id, dst_id)
+        temp_time_specific_lambda = time.time() - start
 
         start = time.time()
         lambda_, path_length = make_detour(G, lambda_list, graphs, src_id, dst_id)
@@ -170,28 +187,40 @@ def test_detouring(lambda_list, graph_name="Anaheim", n_of_tests=100):
 
         if path_length_no_lambda_temp != float("inf"):
             path_length_without_lambda += path_length_no_lambda_temp
+
+        if path_l_temp_specific != float("inf"):
+            path_length_specific_lambda += path_l_temp_specific
+
         total_time_without_lambda += temp_time_no_lambda
+        total_time_with_lambda += temp_time_lambda
+        total_time_specific_lambda += temp_time_specific_lambda
 
         if lambda_ == -1 | (float(path_length) == float("inf")) | (float(path_length) == -float("inf")):
             no_path += 1
-            total_time_with_lambda += temp_time_lambda
+
         else:
             path_list.append(path_length)
-            total_time_with_lambda += temp_time_lambda
             path_length_with_lambda += path_length
             distrib.append(lambda_)
     # path_length_with_lambda /= (n_of_tests - no_path)
     # print distrib
-    print "path list",path_list
+    # print "path list",path_list
+    less_than_1 = sum([1 for x in distrib if x <= 1.0])/len(distrib)
+    print "ration of smaller or equal 1: {}".format(less_than_1)
     plt.hist(distrib, bins=len(lambda_list))
     plt.title("Lambda detour for {} network".format(graph_name))
     plt.xlabel("lambda")
     plt.ylabel("# of detours")
     plt.savefig("results_{}.svg".format(graph_name))
     plt.show()
-    print "Graph {}, no detour {}, total time: {}, average path length {}".format(graph_name, no_path,total_time_with_lambda,path_length_with_lambda)
-    print "time without lambda: {}, path length without lambda {}, ration path: {}, ratio time {}".format(total_time_without_lambda,path_length_without_lambda,path_length_with_lambda/path_length_without_lambda,total_time_with_lambda/total_time_without_lambda)
-
+    print "Graph {}, no detour {}".format(graph_name, no_path)
+    print "base         path {} time {}".format(path_length_without_lambda,total_time_without_lambda)
+    print "all lambdas  path {} time {}".format(path_length_with_lambda,total_time_with_lambda)
+    # print "lambda={}    path {} time {}".format(specific_lambda,path_length_specific_lambda,total_time_specific_lambda)
+    print "ratio path: all_lambdas vs. base {}".format(path_length_with_lambda/path_length_without_lambda)
+    # print "ratio path: lambda = {} vs. base {}".format(specific_lambda,path_length_specific_lambda/path_length_without_lambda)
+    print "ratio time: all_lambdas vs. base {}".format(total_time_with_lambda/total_time_without_lambda)
+    # print "ratio time: lambda = {} vs. base {}".format(specific_lambda,total_time_specific_lambda/total_time_without_lambda)
 def test():
     G = snap.TNEANet().New()
     G.AddFltAttrN("Flow")
